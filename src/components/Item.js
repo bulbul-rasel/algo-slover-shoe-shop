618a27710cd60dd0c03874bbf75e5f3e166aa18c
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-06-05 17:38:42 +0600

    add readme file

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,8 +8,7 @@ const Item = ({ item }) => {
             <h4 className='text-xl text-primary font-bold'> Name: {name}</h4>
             <p className='text-xl text-primary font-semibold'>Category: {category}</p>
             <p className='text-xl text-primary font-semibold'>Price: ${price}</p>
-            {/* <p className='text-xl text-primary font-semibold'>Quantity: {quantity}/pcs</p>
-            <p className='text-xl text-primary font-semibold'>Minimum Order Quantity: {minimum}</p> */}
+
             <button className='btn btn-primary my-3' variant="" type="submit">Book now</button>
 
         </div>
